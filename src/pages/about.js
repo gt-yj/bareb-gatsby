394b6e9ae1db7c945a8d55e119c35d4dfb0572b5
git-yj <1024byte.code@gmail.com>
2021-06-01 11:02:42 +0530

    link element

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
   return (
     <div>
       <h1>About Me</h1>
       <p>I'm Yash Jain and Here I'm to test out gatsby build.</p>
-      <p>Need a Developer to make your fancy website? <a href="/contact">Contact Me</a></p>
+      <p>Need a Developer to make your fancy website? <link to="/contact">Contact Me</link></p>
     </div>
   )
 }
