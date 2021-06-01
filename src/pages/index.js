c588cd1dfbcaf45d9f8b2d475fdd589c9e5cad4d
git-yj <1024byte.code@gmail.com>
2021-06-01 10:27:00 +0530

    2-1

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,8 @@ import React from "react"
 const IndexPage = () => {
   return (
     <div>
-      <h1>
-        Hello World
-      </h1>
-      <h3>
-        I'm YJ. I'm here to test out gastby site builder.
-      </h3>
+      <h1>Hello World</h1>
+      <h3>I'm YJ. I'm here to test out gastby site builder.</h3>
     </div>
   )
 }
