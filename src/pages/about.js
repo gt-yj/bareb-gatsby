7dd675dd67c71f12780d0c65a47d2dd9c9c80e7f
git-yj <1024byte.code@gmail.com>
2021-06-01 10:59:30 +0530

    navigation

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ const AboutPage = () => {
     <div>
       <h1>About Me</h1>
       <p>I'm Yash Jain and Here I'm to test out gatsby build.</p>
+      <p>Need a Developer to make your fancy website? <a href="/contact">Contact Me</a></p>
     </div>
   )
 }
