b3d6fbdd2b7b3f6dabbff5a36b373576a0d10da6
git-yj <1024byte.code@gmail.com>
2021-06-01 10:42:12 +0530

    pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>I'm Yash Jain and Here I'm to test out gatsby build.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import react from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>You can DM me on <a href="https://twitter.com/yashjaing" target='blank'>Twitter</a>.</p>
+    </div>
+  )
+}
